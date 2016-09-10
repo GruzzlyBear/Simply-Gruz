2ca4b8f6aef1e32d321334a6ba37aa03f92b94ff
Alexander Griffin <alexander_goudjeb@hotmail.com>
2016-09-10 18:24:19 +0200

    this is what happens when ya copypaste code :>

--- Graphics/MusicWheelItem grades.lua
+++ Graphics/MusicWheelItem grades.lua
@@ -1,6 +1,6 @@
 return Def.ActorFrame{
 	LoadFont("_big") .. {
-        InitCommand=cmd(xy,150,-1;zoom,0.5;maxwidth,WideScale(get43size(20),20)/0.5),
+        InitCommand=cmd(xy,150,-1;zoom,0.5;maxwidth,WideScale(WideScale((640*(20/854)),20)/0.5),
         SetGradeCommand=function(self,params)
 			local sGrade = params.Grade or 'Grade_None'
 			self:valign(0.5)
