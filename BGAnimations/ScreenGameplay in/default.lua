1120cfc504cf42e5b1ef0ef2a450a4aae4e2059c
Matthew Low <GruzzlyBear@gmail.com>
2016-09-16 00:13:24 +1000

    new splash

--- BGAnimations/ScreenGameplay in/default.lua
+++ BGAnimations/ScreenGameplay in/default.lua
@@ -58,19 +58,6 @@ local t = Def.ActorFrame{
 	},
 
 
-	LoadActor("heartsplode")..{
-		InitCommand=cmd(diffusealpha,0),
-		OnCommand=cmd(sleep,0.4; diffuse, GetCurrentColor(); Center; rotationz,10; diffusealpha,0; zoom,0; diffusealpha,0.9; linear,0.6; rotationz,0; zoom,1.1; diffusealpha,0)
-	},
-	LoadActor("heartsplode")..{
-		InitCommand=cmd(diffusealpha,0),
-		OnCommand=cmd(sleep,0.4; diffuse, GetCurrentColor(); Center; rotationy,180; rotationz,-10; diffusealpha,0; zoom,0.2; diffusealpha,0.8; decelerate,0.6; rotationz,0; zoom,1.3; diffusealpha,0)
-	},
-	LoadActor("minisplode")..{
-		InitCommand=cmd(diffusealpha,0),
-		OnCommand=cmd(sleep,0.4; diffuse, GetCurrentColor(); Center; rotationz,10; diffusealpha,0; zoom,0; diffusealpha,1; decelerate,0.8; rotationz,0; zoom,0.9; diffusealpha,0)
-	},
-
 	LoadFont("_big")..{
 		Text=text,
 		InitCommand=cmd(Center; diffusealpha,0; shadowlength,1),
